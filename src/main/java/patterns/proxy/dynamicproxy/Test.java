f034d7fcb1c19089b644b5320d76b994aeca9899
Sephirothxlx <1216083287@qq.com>
2018-08-16 17:12:32 +0800

    proxy patterns

--- src/main/java/patterns/proxy/dynamicproxy/Test.java
+++ src/main/java/patterns/proxy/dynamicproxy/Test.java
@@ -6,13 +6,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
 public class Test {
-	public static void main(String[]args) {
-		InvocationHandler id=new MyInvocationHandler<Student>(new Student());
-		Class<?> s=Proxy.getProxyClass(Person.class.getClassLoader(), new Class<?>[] {Person.class});
+
+	public static void main(String[] args) {
+		InvocationHandler id = new MyInvocationHandler<Student>(new Student());
+		Class<?> s = Proxy.getProxyClass(Person.class.getClassLoader(), new Class<?>[] { Person.class });
 		try {
-			Constructor c=s.getConstructor(InvocationHandler.class);
+			Constructor c = s.getConstructor(InvocationHandler.class);
 			try {
-				Person p=(Person) c.newInstance(id);
+				Person p = (Person) c.newInstance(id);
 			} catch (InstantiationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -33,7 +34,7 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Person p=(Person) Proxy.newProxyInstance(Person.class.getClassLoader(), new Class<?>[] {Person.class}, id);
+		Person p = (Person) Proxy.newProxyInstance(Person.class.getClassLoader(), new Class<?>[] { Person.class }, id);
 		System.out.println(Person.class.getClassLoader().toString());
 		System.out.println(MyInvocationHandler.class.getClassLoader().toString());
 		p.getName();
